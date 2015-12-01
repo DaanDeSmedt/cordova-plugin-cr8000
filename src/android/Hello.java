409e8ee0d6ae89fb315a16b1eeedecec5f516aee
DaanDeSmedt <daan.de.smedt@hotmail.com>
2015-12-01 16:23:22 +0100

    CR8000 integration

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -3,9 +3,10 @@ package com.example.plugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.BroadcastReceiver;
 
 import org.apache.cordova.*;
 import org.json.JSONArray;
@@ -15,24 +16,35 @@ public class Hello extends CordovaPlugin {
 	
 	// INTENTS
 	public static final String ACTION_BCR_TRIGGER = "oem.android.bcr.ACTION_BCR_TRIGGER";
-    public static final String ACTION_BCR_TRIGGER_KEYCODE = "oem.android.bcr.ACTION_BCR_TRIGGER_KEYCODE";
+	public static final String ACTION_BCR_TRIGGER_KEYCODE = "oem.android.bcr.ACTION_BCR_TRIGGER_KEYCODE";
+	public static final String ACTION_FEEDBACK = "oem.android.bcr.ACTION_FEEDBACK";
 	// PHONEGAP ACTIONS 
     private static final String actionStartScanning = "start";
-
+	
+	private CallbackContext scanningCallBackContext;
+	
 	
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("scan")) {
-			
+			// send native device intent to trigger scan start action
 			Intent intent = new Intent(ACTION_BCR_TRIGGER);
 			intent.putExtra(ACTION_BCR_TRIGGER_KEYCODE, 118);
 			this.cordova.getActivity().getApplicationContext().sendBroadcast(intent);
 			
+			// dummy return
             String name = data.getString(0);
             String message = "Hello, " + name;
-            callbackContext.success(message);
-
+			
+			// set callbackcontext
+			scanningCallBackContext = callbackContext;
+			
+			// add intent filter for capturing result
+			IntentFilter filter = new IntentFilter();  
+			filter.addAction(ACTION_FEEDBACK); 
+			registerReceiver(mCodeScanReceiver, filter); 
+			
             return true;
 
         } else {
@@ -41,4 +53,27 @@ public class Hello extends CordovaPlugin {
 
         }
     }
+	
+	public BroadcastReceiver mCodeScanReceiver = new BroadcastReceiver() {
+    	
+    	@Override
+    	public void onReceive(Context context, Intent intent){
+            if (intent.getAction().equals(ACTION_FEEDBACK))
+            { 
+				// succes callback
+				if(scanningCallBackContext != null) {
+					String  szComData = intent.getStringExtra(Intent.EXTRA_TEXT);
+					scanningCallBackContext.success(message);
+				}
+            }
+    	}
+    };
+    
+    @Override
+	public void onDestroy() 
+	{
+	    unregisterReceiver(mCodeScanReceiver);
+	    super.onDestroy();
+	}
+	
 }

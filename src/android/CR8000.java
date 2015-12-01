a62c160e0640035cfdfe6edf56e628dddc56da4d
DaanDeSmedt <daan.de.smedt@hotmail.com>
2015-12-01 15:09:42 +0100

    Update

--- src/android/CR8000.java
+++ src/android/CR8000.java
@@ -1,4 +1,4 @@
-package com.example.plugin;
+package com.sdp.barcodescanner.cr8000;
 
 import org.apache.cordova.*;
 import org.json.JSONArray;
--- www/cr8000.js
+++ www/cr8000.js
@@ -1,7 +1,7 @@
-/*global cordova, module*/
-
 module.exports = {
     scan: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "CR8000", "greet", [name]);
+		alert('scanning invoke action on cr8000.js');
+		cordova.exec(successCallback, errorCallback, "cr8000", "greet", [name]);
+		cordova.exec(successCallback, errorCallback, "CR8000", "greet", [name]);
     }
 };

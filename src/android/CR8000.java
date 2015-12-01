c05b826c34eb15dd2695ca526cf0091560b27b8f
DaanDeSmedt <daan.de.smedt@hotmail.com>
2015-12-01 11:45:11 +0100

    Initial plugin

---
+++ src/android/CR8000.java
@@ -0,0 +1,21 @@
+package com.example.plugin;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class CR8000 extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+
+        if (action.equals("greet")) {
+            String name = data.getString(0);
+            String message = "Hello, " + name;
+            callbackContext.success(message);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

e8a710e57416e6a990f67517a6ff877ef56ac6ac
DaanDeSmedt <daan.de.smedt@hotmail.com>
2015-12-07 17:06:32 +0100

    Mark-up fix

--- www/cr8000.js
+++ www/cr8000.js
@@ -1,7 +1,7 @@
 module.exports = {
 	scan: function (successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "CR8000", "scan", []);
-    }
+    },
 	listen: function (successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "CR8000", "listen", []);
     }
